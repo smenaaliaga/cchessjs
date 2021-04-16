e647a48c6dc92ef3a7054fd6e901b2f8217993a2
Sebastian Mena <smenaaliaga@gmail.com>
2021-04-15 23:18:54 -0400

    Tablero creado con chessboardjs

---
+++ index.js
@@ -0,0 +1,14 @@
+var welcome = `
+----------------------------
+------ Choripan Chess ------
+----------------------------
+Version: 0.1.0a
+Dev: Smenaalaiga
+----------------------------
+`;
+
+console.log(welcome);
+
+fan = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+var board = ChessBoard('board', fan);
